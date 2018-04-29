e432f9dbeb186e237b6a03c4be62ddfdc363998d
dbteku <dbteku@hotmail.com>
2018-04-29 14:14:56 -0600

    Starting on APP

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { connect } from "@boostbank/react-stateful";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = connect(this, store => {
+      this.setState({
+        todo: store.selectedTodo
+      });
+    });
+  }
+
   render() {
+    let todo = null;
+
+    if (this.state.todo && this.state.todo.name) {
+      todo = <h4>Current Todo: {this.state.todo.name}</h4>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            Welcome to react-stateful-stateless example
+          </h1>
+          <code>See how these two can work together in harmony</code>
+          {todo}
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ src/events/GetTodoListEvent.js
@@ -0,0 +1,8 @@
+const id = "GetTodoListEvent";
+
+export default function GetTodoListEvent(list) {
+  return {
+    id,
+    list
+  };
+}

5c24616f54a48ebb82ed6629d8ae6927ae8f8298
dbteku <dbteku@hotmail.com>
2018-04-29 12:45:14 -0600

    Updated to correct dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "@boostbank/stateful";
+import { GlobalState } from "@boostbank/react-stateful";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <GlobalState store={createStore()}>
+    <App />
+  </GlobalState>,
+  document.getElementById("root")
+);
 registerServiceWorker();
